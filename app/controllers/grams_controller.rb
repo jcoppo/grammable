56e7d23d3635efb3e9137d726d5929b509d22d1a
jcoppo <jaysoncoppo@gmail.com>
2019-10-01 06:45:51 +0000

    gram show test

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,6 @@
 class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
     @gram = Gram.new
   end
@@ -7,8 +9,17 @@ class GramsController < ApplicationController
   end
 
   def create
-    @gram = Gram.create(gram_params)
-    redirect_to root_path
+    # @gram = Gram.create(gram_params)
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @gram = Gram.find(params[:id])
   end
 
   private
